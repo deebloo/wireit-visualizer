6a78f45e4ec678e97b4d90418c37e73172d9cc8e
deebloo <dannybluedesign@gmail.com>
2023-12-20 15:25:39 -0500

    expose wireit bits

--- src/lib.ts
+++ src/lib.ts
@@ -1,2 +1,8 @@
 export { Graph } from "./graph.js";
 export { WireitAnalyzer, Analyzer } from "./analyzer.js";
+export {
+  WireitDependency,
+  WireitPackage,
+  WireitTask,
+  WireitTaskConfig,
+} from "./wireit.js";
