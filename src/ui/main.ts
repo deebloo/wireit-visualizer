84003190e90e1aa21ac22c5c068d98e8a4892b19
deebloo <dannybluedesign@gmail.com>
2023-12-21 02:07:40 -0500

    log string version of graph

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -39,5 +39,7 @@ Promise.all([lib, data]).then(([viz, data]) => {
   const svg = viz.renderSVGElement(config);
   const str = viz.renderString(config);
 
+  console.log(str);
+
   document.getElementById("mynetwork")!.appendChild(svg);
 });
